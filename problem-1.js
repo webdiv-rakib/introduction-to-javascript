dc829639095b43a48f476899e09cfe9e75fc0116
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 05:33:06 +0600

    practice problem 1 solve

---
+++ problem-1.js
@@ -0,0 +1,8 @@
+var orangesPrice = 200;
+var applePrice = 150;
+var totalPrice = orangesPrice + applePrice;
+console.log(totalPrice);
+
+var rakib = 1000;
+var willReturn = rakib - totalPrice;
+console.log(willReturn);
