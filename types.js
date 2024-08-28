b2b6eab281d550a509a358aa27eb7358d3108e0a
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-28 22:40:27 +0600

    string type boolean and type example

--- types.js
+++ types.js
@@ -1,2 +1,19 @@
+// String type variable
 var name = "Abu Nayim Mohammad Rakib";
-console.log(name);
+
+var address = "Tangail";
+
+var country = "Bangladesh";
+
+// Boolean type variable
+var isRich = true;
+
+var passed = false;
+console.log(passed);
+console.log(typeof passed)
+console.log(country)
+console.log(typeof country)
+
+var price = 555;
+console.log(price);
+console.log(typeof price);
