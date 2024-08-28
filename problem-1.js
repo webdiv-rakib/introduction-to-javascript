44e2219ae0b46eeab3c5deb201967ccdaf361db3
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 05:38:31 +0600

    problem 2 solve

--- problem-1.js
+++ problem-1.js
@@ -1,8 +1,21 @@
+// Problem-1
 var orangesPrice = 200;
 var applePrice = 150;
 var totalPrice = orangesPrice + applePrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 var rakib = 1000;
 var willReturn = rakib - totalPrice;
-console.log(willReturn);
+// console.log(willReturn);
+
+// Problem-2
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+var sum = mathematics + biology + chemistry + physics + bangla;
+console.log(sum);
+
+var avg = sum / 5;
+console.log(avg.toFixed(2));
