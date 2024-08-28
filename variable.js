126a5861604897ae622dae6eb693b6527d952e58
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-28 21:20:18 +0600

    variable declare and execute

---
+++ variable.js
@@ -0,0 +1,12 @@
+/**
+ * Variable ==> vary + able
+ * 5 things to declare a variable
+ * 
+ */
+var weight = 90;
+var price = 90;
+price = 500;
+var year = 1999;
+var balance = 2005;
+console.log(price);
+console.log(year);
