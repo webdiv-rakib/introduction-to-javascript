7aa98d3d778581cd2c617cf808223e48cf86695b
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-28 20:44:28 +0600

    Math needed for programming

---
+++ math.js
@@ -0,0 +1,14 @@
+// Math needed for programming
+/**1. Number: +ve, -ve, 0 [50 , 0, -9] 
+ * 2. Integer: 2, 88,[odd,even]
+ * 3. Float: 2.5, 2.2
+ * 4. Operation: +, -, *, /.
+ * 5. More Operation: %, [].
+ * 6. Percentage: 5%, 50taka er 10%.
+ * 7. Sum: 
+ * 8. Avg:
+ * 9. Max/Min: 
+ * 10.Series:
+ * 11.Area: Triangle,
+ * 12.
+*/
