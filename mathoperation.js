4d0e9676ed1c352b558eaff0d2a7d86e082616d2
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 05:02:28 +0600

    shorthand example

--- mathoperation.js
+++ mathoperation.js
@@ -2,28 +2,44 @@
 var onionPrice = 20;
 var eggPrice = 10;
 var totalPrice = onionPrice + eggPrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 // Subtraction
 var onionPrice = 20;
 var eggPrice = 10;
 var totalPrice = onionPrice - eggPrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 // Multiplication
 var onionPrice = 20;
 var eggPrice = 10;
 var totalPrice = onionPrice * eggPrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 // Division
 var onionPrice = 20;
 var eggPrice = 10;
 var totalPrice = onionPrice / eggPrice;
-console.log(totalPrice);
+// console.log(totalPrice);
 
 // Modules
 var onionPrice = 5;
 var eggPrice = 2;
 var totalPrice = onionPrice % eggPrice;
-console.log(totalPrice);
+// console.log(totalPrice);
+
+// ShortHand
+var price = 30;
+price = price + 10;
+price += 10;
+// console.log(price);
+
+var eggPrice = 50;
+eggPrice = eggPrice - 5;
+eggPrice -= 5;
+// console.log(eggPrice);
+
+var penPrice = 10;
+// penPrice = penPrice * 20;
+penPrice *= 5;
+console.log(penPrice);
