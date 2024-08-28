0c1374d320a21d275895f2f14f2d190ea6cf0afa
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-08-29 04:48:13 +0600

    math operations js

---
+++ mathoperation.js
@@ -0,0 +1,29 @@
+// Addition
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice + eggPrice;
+console.log(totalPrice);
+
+// Subtraction
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice - eggPrice;
+console.log(totalPrice);
+
+// Multiplication
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice * eggPrice;
+console.log(totalPrice);
+
+// Division
+var onionPrice = 20;
+var eggPrice = 10;
+var totalPrice = onionPrice / eggPrice;
+console.log(totalPrice);
+
+// Modules
+var onionPrice = 5;
+var eggPrice = 2;
+var totalPrice = onionPrice % eggPrice;
+console.log(totalPrice);
